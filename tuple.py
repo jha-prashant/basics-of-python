900cd705773e8be09d5f155406ef25a077a57b9c
prashant jha <prasshantj2002@gmail.com>
2024-04-15 23:27:24 +0530

    latest commit

--- tuple.py
+++ tuple.py
@@ -1,4 +1,4 @@
-'''list =[]
+list =[]
 movie1=input("enter movie1 name")
 movie2=input("enter movie2 name")
 movie3=input("enter movie3 name")
@@ -8,7 +8,7 @@ list.append(movie3)
 print(list)
 
 grade=('c','d','a','a','b','b','a')
-print(grade.count('a'))'''
+print(grade.count('a'))
 
 grading=['c','d','a','a','b','b','a']
 grading.sort()
